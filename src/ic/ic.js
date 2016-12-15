351077eb710939d1475e5c003bdae41ef894d149
hlkjsen <578595193@qq.com>
2016-12-15 13:56:51 +0800

    test

--- src/ic/ic.js
+++ src/ic/ic.js
@@ -1,3 +1,22 @@
-var $ic = function () {
-
+module.exports = function $ic(query) {
+    var icArray = [];
+    if(typeof query == 'string'){
+        if (query[0] == '#'){
+            var dom = document.getElementById(query.substr(1));
+            if(dom){
+                icArray.push(dom);
+            }
+        }else if(query[0] == '.'){
+            var doms = document.getElementsByClassName(query.substr(1));
+            if (doms && doms.length){
+                Array.prototype.push.apply(icArray, doms);
+            }
+        }else {
+            var doms = document.getElementsByTagName(query);
+            if (doms && doms.length){
+                Array.prototype.push.apply(icArray, doms);
+            }
+        }
+        return icArray;
+    }
 };
