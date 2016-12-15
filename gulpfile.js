a836837da2c4e642f6bbc77b9f8ddad0934c6d13
hlkjsen <578595193@qq.com>
2016-12-15 10:14:13 +0800

    add node_modules to gitignore

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// npm install gulp-browserify babelify  gulp-plumber gulp --save-dev
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var plumber = require('gulp-plumber');
+gulp.task('es6ToEs5', function() {
+    return browserify('./src/main.js')
+        .transform(babelify)
+        .bundle()
+        .pipe(plumber())
+        //Pass desired output filename to vinyl-source-stream
+        .pipe(source('bundle.js'))
+        // Start piping stream to tasks!
+        .pipe(gulp.dest('./'));
+});
+
+var uglify = require('gulp-uglify');
+gulp.task('uglify',['es6ToEs5'], function () {
+   return gulp.src('./bundle.js')
+       .pipe(plumber())
+       .pipe(uglify())
+       .pipe(gulp.dest("./"));
+});
+
+
+// npm install --save-dev gulp-watch --save-dev
+var watch = require('gulp-watch');
+gulp.task('watch',['uglify'], function(){
+    watch('./src/**/*.js', function () {
+        gulp.run('uglify');
+    });
+});
