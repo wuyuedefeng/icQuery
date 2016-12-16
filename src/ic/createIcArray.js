cdf669442d04f9998188a8c2286e8727aaceab5e
hlkjsen <578595193@qq.com>
2016-12-16 12:09:42 +0800

    多级查询

---
+++ src/ic/createIcArray.js
@@ -0,0 +1,54 @@
+/**
+ * 将普通数组转换成icArray数组
+ * @param _icArray
+ * @returns {*|Array}
+ */
+function createIcArray(arr) {
+    _icArray = arr || [];
+    /**
+     * 查找元素节点 $ic(query)
+     * $ic('#myId')  $ic('.myClass') $ic('div')
+     * @param query :string
+     * @param rootElement dom default document
+     */
+    _icArray._query = function (query, rootElement) {
+        rootElement = rootElement || document;
+        if (typeof query == 'string') {
+            if (query[0] == '#') {
+                var dom = rootElement.querySelector(query);
+                if (dom) {
+                    this.push(dom);
+                }
+            } else if (query[0] == '.') {
+                var doms = rootElement.querySelectorAll(query);
+                if (doms && doms.length) {
+                    Array.prototype.push.apply(this, doms);
+                }
+            } else {
+                var doms = rootElement.querySelectorAll(query);
+                if (doms && doms.length) {
+                    Array.prototype.push.apply(this, doms);
+                }
+            }
+        }
+        return this;
+    };
+    /**
+     * 查询所有子节点
+     * @param query: string
+     * @returns icArray
+     */
+    _icArray.find = function(query) {
+        var icArray = createIcArray();
+        this.forEach(function (element) {
+            icArray._query(query, element);
+        });
+        return icArray;
+    };
+    _icArray.find.icDesc = '查询所有子孙节点';
+
+    // 初始化方法完毕返回
+    return _icArray;
+};
+
+module.exports = createIcArray;
--- src/ic/ic.js
+++ src/ic/ic.js
@@ -1,22 +1,4 @@
 module.exports = function $ic(query) {
-    var icArray = [];
-    if(typeof query == 'string'){
-        if (query[0] == '#'){
-            var dom = document.querySelector(query);
-            if(dom){
-                icArray.push(dom);
-            }
-        }else if(query[0] == '.'){
-            var doms = document.querySelectorAll(query);
-            if (doms && doms.length){
-                Array.prototype.push.apply(icArray, doms);
-            }
-        }else {
-            var doms = document.querySelectorAll(query);
-            if (doms && doms.length){
-                Array.prototype.push.apply(icArray, doms);
-            }
-        }
-        return icArray;
-    }
+    var icArray = require('./createIcArray')();
+    return icArray._query(query);
 };
